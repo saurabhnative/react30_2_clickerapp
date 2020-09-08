c8622d3192e18563fed484268da7162202f9ab3a
Saurabh Native <codeclassifiers@gmail.com>
2020-09-08 22:51:04 +0530

    assignment code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Clicker from "./components/Clicker";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [title] = useState("Clicker");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title={title} />
+      <Clicker />
     </div>
   );
 }
-
-export default App;
